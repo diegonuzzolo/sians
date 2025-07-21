5ed45f84bdcb61be3f7c6a21bc9881b50f3d9d67
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-22 00:03:46 +0200

    adasd

--- index.php
+++ index.php
@@ -17,15 +17,15 @@ $slotDisponibili = $stmt->fetchColumn();
 </div>
 <div class="bg-dark text-white  text-center py-5" style="height: 35vh; border-radius: 15px; background-image: url('assets/minecraft-bg3.jpg'); background-size: cover; background-position: center;">
 </div>
+<div class="text-center">
     <?php if (!isset($_SESSION['user_id'])): ?>
         
-        <div class="text-center">
             <a href="register.php" class="btn btn-success btn-lg mt-3 me-2">Registrati</a>
             <a href="login.php" class="btn btn-outline-dark btn-lg mt-3">Accedi</a>
-        </div>
-        <?php else: ?>
-            <a href="dashboard.php" class="btn btn-primary btn-lg mt-3">Vai alla Dashboard</a>
-            <?php endif; ?>
+            <?php else: ?>
+                <a href="dashboard.php" class="btn btn-primary btn-lg mt-3">Vai alla Dashboard</a>
+                <?php endif; ?>
+            </div>
     
     
     
