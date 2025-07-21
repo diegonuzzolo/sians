ad9fa4a6b2a4c03e98423096b10c1bc55604539f
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-21 21:20:29 +0200

    asd

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $slotDisponibili = $stmt->fetchColumn();
 <?php include 'includes/header.php'; ?>
 
 <!-- Hero -->
-<div class="bg-dark text-white  text-center " style="border-radius: 15px; background-image: url('assets/minecraft-bg2.jpg'); background-size: cover; background-position: center;">
+<div class="bg-dark text-white  text-center py-5" style="border-radius: 15px; background-image: url('assets/minecraft-bg2.jpg'); background-size: cover; background-position: center;">
     <div class="container">
         <h1 class="display-4 fw-bold ">Crea il tuo Server Minecraft</h1>
         <p style=" padding: 5px; margin: 10px; font-weight: bold; text-align: start;" class="lead fs-2">Hosting veloce </p><i class="bi-check-circle-fill text-success"></i>
