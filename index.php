92d6bf3cbb35ec28250949e9da479852bc20673f
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-22 08:44:08 +0200

    aggiornamento banner

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ $slotDisponibili = $stmt->fetchColumn();
 <div class="container">
     <h1 style="text-shadow: 2px 2px 4px; color: black;" class="display-4 fw-bold ">Crea il tuo Server Minecraft</h1>
 </div>
-<div class="bg-dark text-white  text-center py-5" style="height: 50vh; border-radius: 15px; background-image: url('assets/banner.png'); background-size: cover; background-position: center;">
+<div class="bg-dark text-white  text-center py-5" style="height: 100vh; border-radius: 15px; background-image: url('assets/banner.png'); background-size: cover; background-position: center;">
+    <div class="text-center">
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            
+                <a href="register.php" class="btn btn-success btn-lg mt-3 me-2">Registrati</a>
+                <a href="login.php" class="btn btn-outline-dark btn-lg mt-3">Accedi</a>
+                <?php else: ?>
+                    <a href="dashboard.php" class="btn btn-primary btn-lg mt-3">Vai alla Dashboard</a>
+                    <?php endif; ?>
+                </div>
 </div>
-<div class="text-center">
-    <?php if (!isset($_SESSION['user_id'])): ?>
-        
-            <a href="register.php" class="btn btn-success btn-lg mt-3 me-2">Registrati</a>
-            <a href="login.php" class="btn btn-outline-dark btn-lg mt-3">Accedi</a>
-            <?php else: ?>
-                <a href="dashboard.php" class="btn btn-primary btn-lg mt-3">Vai alla Dashboard</a>
-                <?php endif; ?>
-            </div>
     
     
     
