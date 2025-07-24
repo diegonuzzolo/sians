24c0a2b64203e52f034d5613fb8e9e397712d49f
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-24 11:30:02 +0200

    d

--- add_server.php
+++ add_server.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'config/config.php';
 require 'includes/auth.php';
 require 'includes/functions.php';
