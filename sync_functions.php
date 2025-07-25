1e047a7a04010363ecc8b01c69b0e329f5c3f667
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-25 11:34:55 +0200

    done

--- sync_functions.php
+++ sync_functions.php
@@ -1,4 +1,14 @@
 <?php
+require("config/config.php");
+
+try {
+    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4", DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Errore connessione DB: " . $e->getMessage());
+}
+
 function syncModpack(array $modpack) {
     global $pdo;
 
