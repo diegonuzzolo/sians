c169a824a81842f8ae46f32057a94e2fdc5397a1
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-25 23:58:23 +0200

    hjbjk

--- add_server.php
+++ add_server.php
@@ -83,21 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="https://sians.it">Minecraft Hosting</a>
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav ms-auto">
-        <?php if (isset($_SESSION['user_id'])): ?>
-          <li class="nav-item"><a class="nav-link" href="../dashboard.php">Dashboard</a></li>
-          <li class="nav-item"><a class="nav-link" href="../logout.php">Logout</a></li>
-        <?php else: ?>
-          <li class="nav-item"><a class="nav-link" href="../login.php">Login</a></li>
-        <?php endif; ?>
-      </ul>
-    </div>
-  </div>
-</nav>
+
 <div class="container mt-4">
 
 <style>
