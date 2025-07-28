291cc7af9238760a3334e178256ec447f042cca9
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-28 14:26:28 +0200

    asdsad

--- check_lock.php
+++ check_lock.php
@@ -1,12 +1,22 @@
 <?php
+header('Content-Type: application/json');
+
 if (!isset($_GET['server_id'])) {
     echo json_encode(['error' => 'Missing server_id']);
     exit;
 }
 
 $serverId = preg_replace('/[^a-zA-Z0-9_-]/', '', $_GET['server_id']);
-$lockFile = "/home/diego/install.lock";
+$logFile = "/home/diego/install_{$serverId}.log";
+
+if (!file_exists($logFile)) {
+    echo json_encode(['exists' => false, 'content' => '']);
+    exit;
+}
+
+$content = file_get_contents($logFile);
 
 echo json_encode([
-    'installing' => file_exists($lockFile)
+    'exists' => true,
+    'content' => $content
 ]);
