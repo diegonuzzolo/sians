5205029fc18f54c40c41afb815e72eddc74372e7
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-30 12:58:27 +0200

    dsasa

--- add_server.php
+++ add_server.php
@@ -79,8 +79,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
                 $sshCmd = "ssh -i /var/www/.ssh/id_rsa -o StrictHostKeyChecking=no diego@$ip " .
-            escapeshellarg("bash /home/diego/setup_server.sh $postType $remoteVersionOrSlug $remoteUrl $remoteMethod $serverId $postVersion") .
-            " > /dev/null 2>&1 &";
+    escapeshellarg("bash /home/diego/setup_server.sh $postType $remoteVersionOrSlug $remoteUrl $remoteMethod $serverId $remoteGameVersion") .
+    " > /dev/null 2>&1 &";
+
 
 
             exec($sshCmd);
