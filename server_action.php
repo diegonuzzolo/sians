827bfd0a14b61f62009cf0181e22b4f117814bb0
diegonuzzolo <nuzzolo27@gmail.com>
2025-07-31 22:53:56 +0200

    asdsasa

--- server_action.php
+++ server_action.php
@@ -27,6 +27,19 @@ $stmt = $pdo->prepare("SELECT ip FROM minecraft_vms WHERE proxmox_vmid = ?");
 $stmt->execute([$proxmoxVmid]);
 $vm = $stmt->fetch();
 
+$stmt = $pdo->prepare("SELECT type, version, modpack_id FROM servers WHERE id = ?");
+$stmt->execute([$serverId]);
+$server = $stmt->fetch();
+
+if (!$server) {
+    http_response_code(404);
+    exit('❌ Server non trovato');
+}
+
+$type = $server['type'];
+$version = $server['version'];
+$modpackId = $server['modpack_id'];
+
 if (!$vm) {
     http_response_code(404);
     exit('❌ VM non trovata');
@@ -35,8 +48,9 @@ if (!$vm) {
 $ip = $vm['ip'];
 $sshUser = 'diego';
 $privateKeyPath = '/var/www/.ssh/id_rsa';
+$startArgs = "$type $version $serverId";
 
-$remoteCommand = "cd /home/diego/minecraft_servers/{$serverId} && bash " . ($action === 'start' ? 'start.sh' : 'stop.sh');
+$remoteCommand = "cd /home/diego/minecraft_servers/{$serverId} && bash " . ($action === 'start' ? "start.sh $startArgs" : 'stop.sh');
 $sshCommand = sprintf(
     'ssh -i %s -o StrictHostKeyChecking=no %s@%s %s',
     escapeshellarg($privateKeyPath),
