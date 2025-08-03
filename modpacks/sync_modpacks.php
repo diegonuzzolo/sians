628c4fdecfc1913fafe94a9ac68fc44230842718
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-03 23:16:28 +0200

    adas

--- modpacks/sync_modpacks.php
+++ modpacks/sync_modpacks.php
@@ -4,9 +4,11 @@ require __DIR__.'/../config/config.php'; // Connessione PDO in $pdo
 function fetchModpacks($limit = 100, $offset = 0) {
     $facets = urlencode(json_encode([
         ["project_type:modpack"],
-        ["categories:multiplayer, categories:forge"],
-        ["client_side:unsupported"],
+        ["categories:multiplayer"],
+        ["categories:forge"],
+        ["client_side:unsupported"]
     ]));
+
     $url = "https://api.modrinth.com/v2/search?game=minecraft&limit=$limit&offset=$offset&facets=$facets";
 
     $curl = curl_init($url);
@@ -25,6 +27,7 @@ function fetchModpacks($limit = 100, $offset = 0) {
     $json = json_decode($response, true);
     return $json['hits'] ?? [];
 }
+
 
 
 function insertOrUpdateModpack($pdo, $modpack) {
