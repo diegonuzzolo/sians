92a8eed0a72275f7042a6c77d4d514a8561f7524
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 20:44:28 +0200

    asd

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,17 @@
 </div> <!-- chiude container -->
+
+<footer>
+  <div class="container mt-5 mb-4 text-center text-light bg-dark p-4 rounded">
+    <h5 class="mb-3 text-warning">Contatti</h5>
+    <p>Sians - Server Minecraft Bedrock Hosting</p>
+    <p>Telefono: <a href="tel:+393713936694" class="text-warning">371 393 6694</a></p>
+    <p>Email: <a href="mailto:info@sians.it" class="text-warning">info@sians.it</a></p>
+    <p>Pagamento sicuro con <strong>Stripe</strong></p>
+  </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../assets/js/script.js"></script>
-<footer>
-    
-    <!-- Sezione Contatti -->
-    <div class="container mt-5 mb-4 text-center text-light bg-dark p-4 rounded">
-        <h5 class="mb-3 text-warning">Contatti</h5>
-        <p>Sians - Server Minecraft Bedrock Hosting</p>
-        <p>Telefono: <a href="tel:+393713936694" class="text-warning">371 393 6694</a></p>
-        <p>Email: <a href="mailto:info@sians.it" class="text-warning">info@sians.it</a></p>
-        <p>Pagamento sicuro con <strong>Stripe</strong></p>
-    </div>
-</footer>
-</body>
 
+</body>
 </html>
