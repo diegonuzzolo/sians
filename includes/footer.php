461d797529d8ad07c7a80054ca61821caf232018
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 20:46:05 +0200

    asd

--- includes/footer.php
+++ includes/footer.php
@@ -12,6 +12,6 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../assets/js/script.js"></script>
-
+<style>footer {position: absolute; bottom: 0;}</style>
 </body>
 </html>
