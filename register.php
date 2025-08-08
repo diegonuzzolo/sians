829a612eddd28938ea138a368cec220f26520970
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 22:28:22 +0200

    sad

--- register.php
+++ register.php
@@ -28,6 +28,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <div class="container mt-5" style="max-width: 500px;">
     <h2 class="mb-4 text-center">Registrati</h2>
@@ -65,3 +67,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 
 <?php require 'includes/footer.php'; ?>
+
+    <style>
+        footer {
+            position: absolute;
+            bottom: 0%;
+            width: 100%;
+        }
+    </style>
