c3f77d219693621a1868796bf69076781bcf025e
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 22:24:36 +0200

    asd

--- login.php
+++ login.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <div class="container mt-5">
     <div class="row justify-content-center">
