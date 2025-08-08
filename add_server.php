cb42783ee1ed55a4e3d79602e0e1e53eccef2a96
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 16:55:54 +0200

    asd

--- add_server.php
+++ add_server.php
@@ -2,9 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 $_GET['server_id'] = $_GET['id'] ?? null; // Se usi un parametro diverso da server_id
 include("auth_check.php");
-session_start();
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
