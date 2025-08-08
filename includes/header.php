297c82b2d9b637fb249845f1dca044c11ff60f07
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-08 21:42:55 +0200

    asd

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet"/>
 
-    <link rel="stylesheet"x href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <style>
         <style>
     body {
