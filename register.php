192c9e875b2a122df63c6fb5192cc4edbb846723
diegonuzzolo <nuzzolo27@gmail.com>
2025-08-09 18:35:59 +0200

    asd

--- register.php
+++ register.php
@@ -29,7 +29,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-  
+<header>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet"/>
+
+</header>
 <div class="main-container">
   <div class="card-custom">
     <h2>Registrati</h2>
